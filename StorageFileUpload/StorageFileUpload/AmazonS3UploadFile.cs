4ed4836a2d0da2f4fcef7b0052f71f7a5bfc10cf
madedanilonovais <danilo.novais@madeinweb.com.br>
2020-01-17 12:55:23 -0300

    sample of codes to upload at azure and aws using c#

---
+++ StorageFileUpload/StorageFileUpload/AmazonS3UploadFile.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace StorageFileUpload
+{
+    public class AmazonS3UploadFile
+    {
+        private static AmazonS3Client _s3Client;
+        private static string _AwsBucketName;
+
+        public DocumentService()
+        {
+            /* Environment Variables to set (sample):
+             * "AWS_ACCESS_KEY_ID": "--AKIA5J71TNDA53PISACAUXJ",
+             * "AWS_SECRET_ACCESS_KEY": "KaPS/59PPOSUGm1SowWxu0iQFL5jPhJcBXZAPXQoQ89Vt",
+             */
+
+            var AwsBucketName = Environment.GetEnvironmentVariable("AWS_BUCKET_NAME");
+
+            var hasErrors = new List<string>();
+
+            if (AwsBucketName == null)
+                hasErrors.Add($"{nameof(AwsBucketName)} variável de ambiente não pode ser nula");
+
+            if (hasErrors.Count > 0)
+                throw new InternalServerError(string.Join(" | ", hasErrors.ToArray()));
+
+            _AwsBucketName = AwsBucketName;
+
+            var credentials = new EnvironmentVariablesAWSCredentials();
+            Amazon.RegionEndpoint region = Amazon.RegionEndpoint.USEast1;
+            _s3Client = new AmazonS3Client(credentials, region);
+        }
+
+        public async Task<string> UploadDocument(string fileNameKey, IFormFile document)
+        {
+            return await AwsStorageUpload(fileNameKey, document);
+        }
+
+        public string GetDocumentUrl(string fileNameKey)
+        {
+            return AwsStorageGetUrl(fileNameKey);
+        }
+
+        private async Task<string> AwsStorageUpload(string fileNameKey, IFormFile docFile)
+        {
+            using (var fileToUpload = docFile.OpenReadStream())
+            {
+                var uploadRequest = new PutObjectRequest
+                {
+                    BucketName = _cashTagAwsBucketName,
+                    InputStream = fileToUpload,
+                    Key = fileNameKey
+                };
+
+                var response = await _s3Client.PutObjectAsync(uploadRequest);
+                return fileNameKey;
+            }
+        }
+
+        private string AwsStorageGetUrl(string fileNameKey)
+        {
+            var getObjectRequest = new GetPreSignedUrlRequest
+            {
+                BucketName = _cashTagAwsBucketName,
+                Key = fileNameKey,
+                Expires = DateTime.UtcNow.AddDays(1)
+            };
+
+            var url = _s3Client.GetPreSignedURL(getObjectRequest);
+            return url;
+        }
+    }
+}
---
+++ StorageFileUpload/StorageFileUpload/AzureUploadFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Services.Samples.AzureUpload
+{
+    public class AzureUploadFile
+    {
+        private async Task<string> AzureStorageUploadSample(string fileName, IFormFile document)
+        {
+            var internalServerErrorMessage = "Erro ao tentar efetuar upload de arquivo";
+
+            try
+            {
+                var BLOB_CONNECTION_STRING = Environment.GetEnvironmentVariable("BLOB_CONNECTION_STRING");
+                var BLOB_CONTAINER_NAME = Environment.GetEnvironmentVariable("BLOB_CONTAINER_NAME");
+
+                var hasErrors = new List<string>();
+
+                if (BLOB_CONNECTION_STRING == null)
+                    hasErrors.Add($"{nameof(BLOB_CONNECTION_STRING)} variável de ambiente não pode ser nula");
+
+                if (BLOB_CONTAINER_NAME == null)
+                    hasErrors.Add($"{nameof(BLOB_CONTAINER_NAME)} variável de ambiente não pode ser nula");
+
+                if (hasErrors.Count > 0)
+                    throw new InternalServerError(string.Join(" | ", hasErrors.ToArray()));
+
+                var storageAccount = CloudStorageAccount.Parse(BLOB_CONNECTION_STRING);
+                var myClient = storageAccount.CreateCloudBlobClient();
+                var container = myClient.GetContainerReference(BLOB_CONTAINER_NAME);
+                var blockBlob = container.GetBlockBlobReference(fileName);
+
+                using (var fileStream = document.OpenReadStream())
+                    await blockBlob.UploadFromStreamAsync(fileStream);
+
+                return blockBlob.SnapshotQualifiedStorageUri.PrimaryUri.ToString();
+            }
+            catch (Exception e) when (!(e is BasicException))
+            {
+                throw new InternalServerError(internalServerErrorMessage);
+            }
+        }
+    }
+}
